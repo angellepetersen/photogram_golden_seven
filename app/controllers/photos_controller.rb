7ad80d9cc673ec72e9e0d2611b9e28d8fc8af394
angellepetersen <apetersen2016@kellogg.northwestern.edu>
2015-05-10 15:58:26 -0500

    Finished the homework!
    Yay

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,38 @@ class PhotosController < ApplicationController
   end
 
   def show
-    @photo_id = Photo.find(params[:id])
+    @photo = Photo.find(params[:id])
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    pic = Photo.new
+    pic.caption = params[:the_caption]
+    pic.source = params[:the_source]
+    pic.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    pic = Photo.find(params[:id])
+    pic.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find(params[:id])
+  end
+
+  def update_row
+    pic = Photo.find(params[:id])
+    pic.source = params[:the_source]
+    pic.caption = params[:the_caption]
+    pic.save
+    redirect_to("http://localhost:3000/photos/#{pic.id}")
   end
 
 end
